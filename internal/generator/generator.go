dbc75eeefa20fb70f6b847eb5a7974955facf747
Asunaro276 <ryuhei276276@gmail.com>
2025-04-27 02:33:49 +0900

    [update] ユーザー生成機能を決定論的にするために乱数生成器を修正

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -2,10 +2,8 @@ package generator
 
 import (
 	"context"
-	"crypto/rand"
 	"encoding/json"
 	"fmt"
-	"math/big"
 	mathrand "math/rand"
 	"os"
 	"path/filepath"
@@ -107,14 +105,15 @@ func readLines(filename string) ([]string, error) {
 	return result, nil
 }
 
-// generateUsers は指定された数のユーザーを生成
+// Generate は指定された数のユーザーを生成
 func (g *Generator) Generate(count int, seed int64, page int, gender string) (string, error) {
-	mathrand.New(mathrand.NewSource(seed))
+	// 乱数ジェネレーターの初期化 - これにより決定論的な結果が得られる
+	rnd := mathrand.New(mathrand.NewSource(seed))
 
 	// ユーザー生成
 	users := make([]map[string]interface{}, count)
 	for i := 0; i < count; i++ {
-		users[i] = g.generateUser(gender)
+		users[i] = g.generateUser(gender, rnd)
 	}
 
 	// レスポンス形式
@@ -139,10 +138,10 @@ func (g *Generator) Generate(count int, seed int64, page int, gender string) (st
 }
 
 // generateUser は1人のユーザーを生成
-func (g *Generator) generateUser(gender string) map[string]interface{} {
+func (g *Generator) generateUser(gender string, rnd *mathrand.Rand) map[string]interface{} {
 	// ユーザーデータの生成
 	if gender == "" {
-		if mathrand.Intn(2) == 1 {
+		if rnd.Intn(2) == 1 {
 			gender = "male"
 		} else {
 			gender = "female"
@@ -155,30 +154,30 @@ func (g *Generator) generateUser(gender string) map[string]interface{} {
 	// 名前の生成
 	if gender == "male" && g.Data["first_names_male"] != nil {
 		firstNames := g.Data["first_names_male"].([]string)
-		firstName = firstNames[mathrand.Intn(len(firstNames))]
+		firstName = firstNames[rnd.Intn(len(firstNames))]
 	} else if gender == "female" && g.Data["first_names_female"] != nil {
 		firstNames := g.Data["first_names_female"].([]string)
-		firstName = firstNames[mathrand.Intn(len(firstNames))]
+		firstName = firstNames[rnd.Intn(len(firstNames))]
 	} else {
 		// デフォルト名 (国籍データがない場合)
 		maleNames := []string{"John", "Robert", "Michael", "David", "William"}
 		femaleNames := []string{"Mary", "Patricia", "Jennifer", "Linda", "Elizabeth"}
 
 		if gender == "male" {
-			firstName = maleNames[mathrand.Intn(len(maleNames))]
+			firstName = maleNames[rnd.Intn(len(maleNames))]
 		} else {
-			firstName = femaleNames[mathrand.Intn(len(femaleNames))]
+			firstName = femaleNames[rnd.Intn(len(femaleNames))]
 		}
 	}
 
 	// 姓の生成
 	if g.Data != nil && g.Data["last_names"] != nil {
 		lastNames := g.Data["last_names"].([]string)
-		lastName = lastNames[mathrand.Intn(len(lastNames))]
+		lastName = lastNames[rnd.Intn(len(lastNames))]
 	} else {
 		// デフォルト姓 (国籍データがない場合)
 		defaultLastNames := []string{"Smith", "Johnson", "Williams", "Brown", "Jones"}
-		lastName = defaultLastNames[mathrand.Intn(len(defaultLastNames))]
+		lastName = defaultLastNames[rnd.Intn(len(defaultLastNames))]
 	}
 
 	// タイトル
@@ -190,16 +189,16 @@ func (g *Generator) generateUser(gender string) map[string]interface{} {
 	// 写真番号
 	var photoNumber int
 	if gender == "male" {
-		photoNumber = mathrand.Intn(47)
+		photoNumber = rnd.Intn(47)
 	} else {
-		photoNumber = mathrand.Intn(25)
+		photoNumber = rnd.Intn(25)
 	}
 
 	// メールアドレス
 	email := strings.ToLower(firstName) + "." + strings.ToLower(lastName) + "@example.com"
 
-	// ユーザーID生成
-	userID, _ := rand.Int(rand.Reader, big.NewInt(100000000))
+	// ユーザーID生成 - 決定論的に生成
+	userID := rnd.Int63n(100000000)
 
 	// プロフィール画像のパス
 	bucket := os.Getenv("BUCKET_NAME")
@@ -208,7 +207,7 @@ func (g *Generator) generateUser(gender string) map[string]interface{} {
 	}
 	thumbnailKey := fmt.Sprintf("%s/portrait (%d).png", gender, photoNumber)
 
-	// 署名付きURL（期限24時間）
+	// 署名付きURL
 	thumbnailURL, _ := generateSignedURL(bucket, thumbnailKey, 10*time.Minute)
 
 	// エラー処理（本番環境では適切に処理すること）
@@ -228,41 +227,41 @@ func (g *Generator) generateUser(gender string) map[string]interface{} {
 		},
 		"location": map[string]interface{}{
 			"street": map[string]interface{}{
-				"number": mathrand.Intn(9999) + 1,
-				"name":   generateRandomStreet(),
+				"number": rnd.Intn(9999) + 1,
+				"name":   generateRandomStreetWithRand(rnd),
 			},
-			"city":     generateRandomCity(),
-			"state":    generateRandomState(),
+			"city":     generateRandomCityWithRand(rnd),
+			"state":    generateRandomStateWithRand(rnd),
 			"country":  "US",
-			"postcode": fmt.Sprintf("%05d", mathrand.Intn(99999)),
+			"postcode": fmt.Sprintf("%05d", rnd.Intn(99999)),
 			"coordinates": map[string]interface{}{
-				"latitude":  fmt.Sprintf("%.4f", -90.0+mathrand.Float64()*180.0),
-				"longitude": fmt.Sprintf("%.4f", -180.0+mathrand.Float64()*360.0),
+				"latitude":  fmt.Sprintf("%.4f", -90.0+rnd.Float64()*180.0),
+				"longitude": fmt.Sprintf("%.4f", -180.0+rnd.Float64()*360.0),
 			},
 		},
 		"email": email,
 		"login": map[string]interface{}{
-			"uuid":     generateUUID(),
-			"username": strings.ToLower(firstName + lastName + strconv.Itoa(mathrand.Intn(99))),
-			"password": generateRandomPassword(),
-			"salt":     generateRandomString(16),
-			"md5":      generateRandomString(32),
-			"sha1":     generateRandomString(40),
-			"sha256":   generateRandomString(64),
+			"uuid":     generateUUIDWithRand(rnd),
+			"username": strings.ToLower(firstName + lastName + strconv.Itoa(rnd.Intn(99))),
+			"password": generateRandomPasswordWithRand(rnd),
+			"salt":     generateRandomStringWithRand(rnd, 16),
+			"md5":      generateRandomStringWithRand(rnd, 32),
+			"sha1":     generateRandomStringWithRand(rnd, 40),
+			"sha256":   generateRandomStringWithRand(rnd, 64),
 		},
 		"dob": map[string]interface{}{
-			"date": time.Now().AddDate(-mathrand.Intn(80)-18, -mathrand.Intn(12), -mathrand.Intn(28)).Format(time.RFC3339),
-			"age":  mathrand.Intn(80) + 18,
+			"date": time.Now().AddDate(-rnd.Intn(80)-18, -rnd.Intn(12), -rnd.Intn(28)).Format(time.RFC3339),
+			"age":  rnd.Intn(80) + 18,
 		},
 		"registered": map[string]interface{}{
-			"date": time.Now().AddDate(-mathrand.Intn(20), -mathrand.Intn(12), -mathrand.Intn(28)).Format(time.RFC3339),
-			"age":  mathrand.Intn(20),
+			"date": time.Now().AddDate(-rnd.Intn(20), -rnd.Intn(12), -rnd.Intn(28)).Format(time.RFC3339),
+			"age":  rnd.Intn(20),
 		},
-		"phone": fmt.Sprintf("(%03d)-%03d-%04d", mathrand.Intn(1000), mathrand.Intn(1000), mathrand.Intn(10000)),
-		"cell":  fmt.Sprintf("(%03d)-%03d-%04d", mathrand.Intn(1000), mathrand.Intn(1000), mathrand.Intn(10000)),
+		"phone": fmt.Sprintf("(%03d)-%03d-%04d", rnd.Intn(1000), rnd.Intn(1000), rnd.Intn(10000)),
+		"cell":  fmt.Sprintf("(%03d)-%03d-%04d", rnd.Intn(1000), rnd.Intn(1000), rnd.Intn(10000)),
 		"id": map[string]interface{}{
 			"name":  "ID",
-			"value": fmt.Sprintf("%08d", userID.Int64()),
+			"value": fmt.Sprintf("%08d", userID),
 		},
 		"picture": map[string]interface{}{
 			"large":     largeURL,
@@ -273,46 +272,48 @@ func (g *Generator) generateUser(gender string) map[string]interface{} {
 	}
 }
 
-// ヘルパー関数
-func generateRandomStreet() string {
+// 決定論的なヘルパー関数
+func generateRandomStreetWithRand(rnd *mathrand.Rand) string {
 	streets := []string{"Main Street", "Park Avenue", "Oak Street", "Maple Avenue", "Cedar Road"}
-	return streets[mathrand.Intn(len(streets))]
+	return streets[rnd.Intn(len(streets))]
 }
 
-func generateRandomCity() string {
+func generateRandomCityWithRand(rnd *mathrand.Rand) string {
 	cities := []string{"New York", "Los Angeles", "Chicago", "Houston", "Phoenix"}
-	return cities[mathrand.Intn(len(cities))]
+	return cities[rnd.Intn(len(cities))]
 }
 
-func generateRandomState() string {
+func generateRandomStateWithRand(rnd *mathrand.Rand) string {
 	states := []string{"California", "New York", "Texas", "Florida", "Illinois"}
-	return states[mathrand.Intn(len(states))]
+	return states[rnd.Intn(len(states))]
 }
 
-func generateUUID() string {
+// シード値に依存したUUIDを生成
+func generateUUIDWithRand(rnd *mathrand.Rand) string {
 	uuid := make([]byte, 16)
-	_, _ = rand.Read(uuid)
+	for i := range uuid {
+		uuid[i] = byte(rnd.Intn(256))
+	}
+	// RFC 4122 バージョン4 UUID形式に準拠
 	uuid[8] = uuid[8]&^0xc0 | 0x80
 	uuid[6] = uuid[6]&^0xf0 | 0x40
 	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:])
 }
 
-func generateRandomPassword() string {
+func generateRandomPasswordWithRand(rnd *mathrand.Rand) string {
 	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	result := make([]byte, 12)
 	for i := range result {
-		n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
-		result[i] = chars[n.Int64()]
+		result[i] = chars[rnd.Intn(len(chars))]
 	}
 	return string(result)
 }
 
-func generateRandomString(length int) string {
+func generateRandomStringWithRand(rnd *mathrand.Rand, length int) string {
 	const chars = "abcdef0123456789"
 	result := make([]byte, length)
 	for i := range result {
-		n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
-		result[i] = chars[n.Int64()]
+		result[i] = chars[rnd.Intn(len(chars))]
 	}
 	return string(result)
 }
